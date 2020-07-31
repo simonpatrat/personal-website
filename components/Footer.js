ac97169975124d44c7af335d37a578328da6b6ff
simonpatrat <simon.patrat@gmail.com>
2020-07-31 18:08:39 -0400

    - Adjust footer size

--- components/Footer.js
+++ components/Footer.js
@@ -3,39 +3,41 @@ import styled from "styled-components";
 export function Footer({ className, ...props }) {
   return (
     <footer className={className}>
-      <div className="container">
-        <div className="row center-xs middle-xs">
-          <div className="col-xs">
-            <ul className="social-links-list">
-              <li className="social-links-list__item">
-                <a
-                  href="https://github.com/simonpatrat"
-                  target="_blank"
-                  title="Compte Github de Simon Patrat"
-                >
-                  <span aria-hidden className="lab la-github" />
-                </a>
-              </li>
-              <li className="social-links-list__item">
-                <a
-                  href="https://www.linkedin.com/in/simon-patrat-07260a110/"
-                  target="_blank"
-                  title="Profil Linkedin de Simon Patrat"
-                >
-                  <span aria-hidden className="lab la-linkedin-in" />
-                </a>
-              </li>
-              <li className="social-links-list__item">
-                <a
-                  href="https://codepen.io/simonpatrat"
-                  target="_blank"
-                  title="Compte Codepen de Simon Patrat"
-                >
-                  <span aria-hidden className="lab la-codepen" />
-                </a>
-              </li>
-            </ul>
-            <div>Fait avec {"\u{1F69C}"} @ Montréal</div>
+      <div className="footer__inner">
+        <div className="container">
+          <div className="row center-xs middle-xs">
+            <div className="col-xs">
+              <ul className="social-links-list">
+                <li className="social-links-list__item">
+                  <a
+                    href="https://github.com/simonpatrat"
+                    target="_blank"
+                    title="Compte Github de Simon Patrat"
+                  >
+                    <span aria-hidden className="lab la-github" />
+                  </a>
+                </li>
+                <li className="social-links-list__item">
+                  <a
+                    href="https://www.linkedin.com/in/simon-patrat-07260a110/"
+                    target="_blank"
+                    title="Profil Linkedin de Simon Patrat"
+                  >
+                    <span aria-hidden className="lab la-linkedin-in" />
+                  </a>
+                </li>
+                <li className="social-links-list__item">
+                  <a
+                    href="https://codepen.io/simonpatrat"
+                    target="_blank"
+                    title="Compte Codepen de Simon Patrat"
+                  >
+                    <span aria-hidden className="lab la-codepen" />
+                  </a>
+                </li>
+              </ul>
+              <div>Fait avec {"\u{1F69C}"} @ Montréal</div>
+            </div>
           </div>
         </div>
       </div>
@@ -44,10 +46,18 @@ export function Footer({ className, ...props }) {
 }
 
 export default styled(Footer)`
-  min-height: 300px;
+  --footer-height: 300px;
+
+  min-height: var(--footer-height);
   display: flex;
   align-items: center;
   justify-content: center;
+
+  .footer__inner {
+    width: 100%;
+    min-height: var(--footer-height);
+    max-width: 1280px;
+  }
 
   .social-links-list {
     list-style: none;
--- pages/about.js
+++ pages/about.js
@@ -7,14 +7,28 @@ const About = ({ title, description, ...props }) => {
         <div className="container">
           <div className="row">
             <div className="col-xs">
-              <h1 className="title">Welcome to my blog!</h1>
+              <h1 className="title">À propos de moi</h1>
 
               <p className="description">{description}</p>
 
               <p>
-                I am a very exciting person. I know this because I'm following a
-                very exciting tutorial, and a not-exciting person wouldn't do
-                that.
+                Diplômé d’une maîtrise en conception de projets multimédias,
+                avec une dizaine d’années d’expérience en TI dont 7 années
+                d’expérience en tant que développeur front-end. Motivé par
+                l’implication dans les phases d’architecture et par
+                l’apprentissage continu et l’amélioration des pratiques liées à
+                ma fonction. J’ai eu l’occasion de participer à des projets de
+                différentes envergures allant du site vitrine au e-commerce en
+                passant par le développement de solutions vouées à
+                l’industrialisation ainsi que les plateformes de billeterie
+                électronique ou ls sites de presse d'information à très grande
+                audience.
+              </p>
+              <p>
+                Passionné, je crois profondément que chaque projet passe par la
+                mise en place d’un système maintenable et évolutif afin de
+                servir au mieux les objectifs tout en anticipant l’évolution des
+                besoins.
               </p>
             </div>
           </div>
